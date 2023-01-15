cbc7a885e54c85e4a662da61358956c49875bff1
ankita-barad <ankitabarad2111@gmail.com>
2023-01-15 13:00:16 +0530

    prime_code

---
+++ prime.js
@@ -0,0 +1,11 @@
+function identifyPrime(num) {
+   
+  for(let i=2;i<num;i--){//have to increment 
+      if(num%i===0){
+          console.log("No")
+         return;
+         }
+  }
+  console.log("Yes")
+  
+}
